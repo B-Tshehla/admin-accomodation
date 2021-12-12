b11c2e4fb7dd5ae5d014d201a1afac1b1f8ab03c
Boitumelo <boitumelotshehla@gmaail.com>
2021-12-12 20:41:24 +0200

    v1.0.10

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,12 +1,105 @@
 <template>
     <div>
          <div class="auth-wrapper-home">
-            <div class="auth-inner-home">
-                <h3>Current Users</h3>
-                <div>
-                    <b-table striped hover :items="items" :fields="fields"></b-table>
+            <div class="auth-inner-home"> 
+                <div v-if="!isSelected">
+                  <h3>Current Users</h3>
+                    <b-table class="table" 
+                    striped hover 
+                    :items="items" 
+                    :fields="fields"
+                    :select-mode="selectMode"
+                    responsive="sm"
+                    ref="selectableTable"
+                    selectable
+                    @row-selected="onRowSelected"
+                    ></b-table>
                 </div>
-                 <b-button block variant="primary" @click="handleSubmit">Test Render</b-button>
+
+                <div v-if="isSelected" class="container">
+                  <h3>Selected Student</h3>
+                  <div class="row align-items-start">
+                    <div class="col">
+                          <h4>Personal Information</h4>
+
+                        <p><b>First Name:</b>
+                          {{selected[0].firstName}}</p>
+                        <p><b>Last Name:</b>
+                          {{selected[0].lastName}}</p>
+                        <p><b>Contact Number:</b>
+                          {{selected[0].conNum}}</p>
+                        <p><b>Identity Number:</b>
+                          {{selected[0].idNum}}</p>
+
+                          <h4>Primary Next Of Keen Information</h4>
+
+                        <p><b>First Name:</b>
+                          {{selected[0].kfName}}</p>
+                        <p><b>Last Name:</b>
+                          {{selected[0].klName}}</p>
+                        <p><b>Contact Number:</b>
+                          {{selected[0].kconNum}}</p>
+                        <p><b>Relationship:</b>
+                          {{selected[0].realation}}</p>
+
+                          <h4>Secondary Next Of Keen Information</h4>
+
+                        <p><b>First Name:</b>
+                          {{selected[0].seckfName}}</p>
+                        <p><b>Last Name:</b>
+                          {{selected[0].secklName}}</p>
+                        <p><b>Contact Number:</b>
+                          {{selected[0].seckconNum}}</p>
+                        <p><b>Relationship:</b>
+                          {{selected[0].secrealation}}</p>
+
+                          <h4>Medical History</h4>  
+                        <p>{{selected[0].medHistory}}</p>
+                          
+                      </div>
+                      <div class="col">
+                        <div v-if="!isDocument">
+                          <h4>Profile Picture</h4>
+                          <b-col>
+                            <b-img thumbnail fluid :src="selected[0].profile" alt="Image 1"></b-img>
+                          </b-col>
+                        </div>
+                        <div v-if="isDocument">
+                          <h4>Uploaded Documents</h4>
+                          <p>
+                            <b>ID Copy: </b>
+                            <a v-bind:href="idCopy"  target="_blank">Document</a>
+                            
+                          </p>
+                          <p>
+                            <b>Proof of Registration:</b>
+                            <a v-bind:href="proofOfReg"  target="_blank">Document</a>
+                            
+                          </p>
+                          <p>
+                            <b>Proof Of Residents:</b>
+                            <a v-bind:href="proofOfAddress"  target="_blank">Document</a>
+                          </p>
+                          <p>
+                            <b>NFSAS Approval Letter:</b>
+                            <a v-bind:href="nfsasApproval"  target="_blank">Document</a>
+                          </p>
+                        </div>
+                         
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col">
+                          <b-button block variant="primary" @click="back">Back</b-button>
+                      </div>
+                      <div class="col">
+                          <b-button block variant="success" @click="uploaded">Documents</b-button>
+                      </div>
+                      
+                    </div>
+                    
+                </div>
+                
             </div>
          </div>
     </div>
@@ -14,31 +107,94 @@
 
 <script>
 import { getFirestore } from "firebase/firestore";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs,doc, getDoc } from "firebase/firestore";
 
 export default {
     name:'home',
-     data() {
-      return {
-        // Note `isActive` is left out and will not appear in the rendered table
-        fields: ['first_name', 'last_name', 'age'],
-        items: [
-          {  age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          {  age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          {  age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
-      }
-    },
-    methods: {
-       async handleSubmit(){
-          const db= getFirestore();
+   async created(){
+      const db= getFirestore();
             const querySnapshot = await getDocs(collection(db, "users"));
             querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
+           //console.log(doc.id, " => ", doc.data());
+            this.items.push(doc.data());
+          
             });
+    },
+     data() {
+      return {
+        // Note `isActive` is left out and will not appear in the rendered table
+        fields: [
+          //'firstName', 
+          {
+            // A column that needs custom formatting,
+            // calling formatter 'fullName' in this app
+            key: 'firstName',
+            label: 'First Name',
+            sortable:true,
+            
+          },
+          {
+            key:'lastName',
+            label: 'Last Name',
+            sortable:true,
+           
+          },
+          {
+            key:'conNum',
+            label:'Contact Number',
+            
+          },
+          {
+            key:'idNum',
+            label:'Identity Number',
+            
+          },
+          
+        ],
+        items: [],
+        studData:[],
+        selectMode: 'single',
+        selected: [],
+        isSelected:false,
+        isDocument:false,
+        idCopy:null,
+        proofOfReg:null,
+        proofOfAddress:null,
+        nfsasApproval:null,
+      }
+    },
+    methods: {
+        back(){
+          this.isSelected=false;
+             
         },
+        onRowSelected(items){
+          
+          console.log(items);
+          this.selected=items;
+          this.isSelected=true;
+          
+        },
+        async uploaded(){
+          const db= getFirestore();
+          const docRef = doc(db, "suporting",this.selected[0].userId);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data());
+            this.idCopy=docSnap.data().idCopy;
+            this.proofOfReg=docSnap.data().proofOfReg;
+            this.proofOfAddress=docSnap.data().proofOfAddress;
+            this.nfsasApproval=docSnap.data().nfsasApproval;
+          } else {
+          // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+
+          this.isDocument=true;
+        }
+          
     },
 }
 </script>
@@ -102,6 +258,10 @@ export default {
   line-height: 1;
   padding-bottom: 20px;
 }
+a{
+  text-decoration: none;
+}
+
 
 
 </style>
